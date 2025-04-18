375fcfd5f011d473e3f9a5ea62591cad5468518a
Jiten <jitendas9574@gmail.com>
2025-04-18 22:17:25 +0530

    write file to filesystem

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const fs = require('fs')
-fs.readFile('example.txt','utf-8',(err,data)=>{
+// fs.readFile('example.txt','utf-8',(err,data)=>{
+//     if (err) {
+//         console.error(error);
+//         return;
+//     }
+//     console.log(data);
+// })
+
+const content = "Hello NodeJS";
+
+fs.writeFile('output.txt',content,(err)=>{
     if (err) {
-        console.error(error);
+        console.error(err);
         return;
     }
-    console.log(data);
+    console.log("file written successfully");
+    
 })
