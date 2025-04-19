148197c873e3be68ba6a1746de92974d45ede316
Jiten <jitendas9574@gmail.com>
2025-04-20 00:51:37 +0530

    Handling Query Parameters

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const http =  require('http')
+const url = require('url')
 
 const server = http.createServer((req,res)=>{
-    if(req.method === 'POST' && req.url === '/submit'){
-        let body = ''
-        req.on('data',(chunk)=>{
-            body += chunk.toString()
-        })
+    if(req.method === 'GET' && req.url.startsWith('/search')){
+        
+        const queryObject = url.parse(req.url, true).query
 
-        req.on('end',()=>{
-            res.writeHead(200,{'content-type':'application/json'})
-            res.end(JSON.stringify({message:"Data received",data:body}))
-        })
+        res.writeHead(200,{'content-type':'application/json',
+                            "custom-header":"node JS server",
+                            "custom-tracking":"1234"})
+        res.end(JSON.stringify({message:"Query received",queryObject}))
     }else{
         res.writeHead(404,{'content-type':'text/plain'})
         res.end('Route Not Found')
