f63f7f81cc44151c52d4cbf5c9f0e1c9bfa011e8
Jiten <jitendas9574@gmail.com>
2025-04-20 01:10:09 +0530

    creating routes

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const http =  require('http')
 const url = require('url')
 
-const server = http.createServer((req,res)=>{
-    if(req.method === 'GET' && req.url.startsWith('/search')){
-        
-        const queryObject = url.parse(req.url, true).query
 
-        res.writeHead(200,{'content-type':'application/json',
-                            "custom-header":"node JS server",
-                            "custom-tracking":"1234"})
-        res.end(JSON.stringify({message:"Query received",queryObject}))
-    }else{
+//routes handler
+
+const routes = {
+    '/':(req,res) =>{
+        res.writeHead(200,{'content-type':'text/plain'})
+        res.end('Welcome to the home page')
+    },
+    '/about':(req,res) =>{
+        res.writeHead(200,{'content-type':'text/plain'})
+        res.end('Welcome to the About page')
+    },
+    '/notfound':(req,res) =>{
         res.writeHead(404,{'content-type':'text/plain'})
-        res.end('Route Not Found')
+        res.end('page not found')
+    }
+}
+const server = http.createServer((req,res)=>{
+    const {pathname}=  url.parse(req.url)
+    if(routes[pathname]){
+        routes[pathname](req,res)
+    }
+    else{
+        routes['/notfound'](req,res)
     }
 })
 
