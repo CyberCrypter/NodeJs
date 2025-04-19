7c081c3d367675a82f9eeebda95d8d8bac8da3f8
Jiten <jitendas9574@gmail.com>
2025-04-19 23:59:29 +0530

    create a Post request

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http =  require('http')
 
 const server = http.createServer((req,res)=>{
-    if(req.method === 'GET' && req.url === '/'){
-        res.writeHead(200,{'content-type':'text/plain'})
-        res.end('Welcome to homepage')
+    if(req.method === 'POST' && req.url === '/submit'){
+        let body = ''
+        req.on('data',(chunk)=>{
+            body += chunk.toString()
+        })
+
+        req.on('end',()=>{
+            res.writeHead(200,{'content-type':'application/json'})
+            res.end(JSON.stringify({message:"Data received",data:body}))
+        })
     }else{
         res.writeHead(404,{'content-type':'text/plain'})
-        res.end('Page Not Found')
+        res.end('Route Not Found')
     }
-
 })
 
 const PORT = 3000
